c1b67d54d7b5ae52e4db0a0727c33f2cbc1d0631
Walter Diaz <wdiaz@cs.columbia.edu>
2021-07-03 02:38:08 -0400

    added find by name

--- src/main/java/com/commerce/inventory/repository/ProductRepository.java
+++ src/main/java/com/commerce/inventory/repository/ProductRepository.java
@@ -4,9 +4,8 @@ import com.commerce.inventory.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findProductBySku(String sku);
+    Product findProductByName(String name);
 }
